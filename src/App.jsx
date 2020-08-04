662f113465761154d1786e01b2427da599e3ed2c
felixoppongwillborg <fwlixw@gmail.com>
2020-08-04 13:37:12 +0200

    added results and if else statement to determine the winner all test pass

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,58 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-const weapons = ["rock", "paper", "scissors"]
+const weapons = ["rock", "paper", "scissors"];
 class App extends Component {
   state = {
     playerChoice: "",
     computer: "",
-  }
+    results: "",
+  };
   onClick(e) {
-   
-    const playerChoice = e.target.id 
-    const computer = weapons[Math.floor(Math.random() * weapons.length)] 
+    const playerChoice = e.target.id;
+    const computer = weapons[Math.floor(Math.random() * weapons.length)];
+    const results = this.getResults(playerChoice, computer);
+    this.setState({
+      playerChoice: playerChoice,
+      computer: computer,
+      results: results,
+    });
   }
-  render () {
-  return (
-    <div>
-      <h1 id="title">Rock Paper Scissors</h1>
-      <button id="rock" onClick={e => this.onClick(e)} >Rock</button>
-      <button id="paper" onClick={e => this.onClick(e)} >Paper</button>
-      <button id="scissors" onClick={e => this.onClick(e)} >Scissors</button>
-    </div>
-  )}
+  getResults(playerChoice, computer) {
+    if (playerChoice === computer) {
+      return "Tie";
+    }
+
+    if (
+      (playerChoice === "rock" && computer === "scissors") ||
+      (playerChoice === "paper" && computer === "rock") ||
+      (playerChoice === "scissors" && computer === "paper")
+    ) {
+      return "You Win";
+    } else {
+      return "You Lose";
+    }
+  }
+  render() {
+    return (
+      <>
+        <div>
+          <h1 id="title">Rock Paper Scissors</h1>
+          <button id="rock" onClick={(e) => this.onClick(e)}>
+            Rock
+          </button>
+          <button id="paper" onClick={(e) => this.onClick(e)}>
+            Paper
+          </button>
+          <button id="scissors" onClick={(e) => this.onClick(e)}>
+            Scissors
+          </button>
+        </div>
+        <div>
+          <h1 id="result">{this.state.results}</h1>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
