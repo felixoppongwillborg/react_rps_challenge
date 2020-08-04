43df9610f71931b0a06b8a891fa0268b7900809b
felixoppongwillborg <fwlixw@gmail.com>
2020-08-04 09:41:45 +0200

    added onClick event and set initial state of playerChoice.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-function App() {
+class App extends Component {
+  state = {
+    playerChoice: "",
+  }
+  onClick(e) {
+   
+    const playerChoice = e.target.id 
+  }
+  render (){
   return (
     <div>
       <h1 id="title">Rock Paper Scissors</h1>
-      <button id="rock">Rock</button>
-      <button id="paper">Paper</button>
-      <button id="scissors">Scissors</button>
+      <button id="rock" onClick={e => this.onClick(e)} >Rock</button>
+      <button id="paper" onClick={e => this.onClick(e)} >Paper</button>
+      <button id="scissors" onClick={e => this.onClick(e)} >Scissors</button>
     </div>
   )}
+}
 
 export default App;
